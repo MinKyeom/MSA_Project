def032f757e24f89561eafd0dc90cf518533f9a2
MinKyeom <minkyeom@naver.com>
2026-02-03 08:48:09 +0900

    MSA Dev Project

---
+++ dev/backend/auth-service/src/main/java/com/mk/auth_service/dto/MailEvent.java
@@ -0,0 +1,13 @@
+package com.mk.auth_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MailEvent {
+    private String email;
+    private String code;
+}
